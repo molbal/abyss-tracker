798e83fb294a78c2966097b0e88e1ae969be8ca7
Balint Molnar <molbal@outlook.com>
2020-01-28 16:29:18 +0100

    New run types migration

--- database/migrations/2020_01_27_125046_extend_runs_info.php
+++ database/migrations/2020_01_27_125046_extend_runs_info.php
@@ -14,13 +14,29 @@ class ExtendRunsInfo extends Migration
     public function up()
     {
         Schema::table("runs",function (Blueprint $t) {
-            $t->bigInteger("ship_id")->nullable()->index();
-            $t->enum("death_reason", [
+            $t->bigInteger("SHIP_ID")->nullable()->index();
+            $t->enum("DEATH_REASON", [
                 'TIMEOUT',
                 'TANK_FAILED',
                 'CONNECTION_DROP',
                 'PILOTING_FAIL',
-            ]);
+                'PVP_DEATH',
+                'OVERHEAT_FAILURE'
+            ])->nullable();
+            $t->boolean("PVP_CONDUIT_USED")->nullable();
+            $t->boolean("PVP_CONDUIT_SPAWN")->nullable();
+            $t->unsignedInteger("FILAMENT_PRICE")->nullable();
+        });
+
+        Schema::create("detailed_loot", function (Blueprint $t) {
+           $t->bigIncrements("ID");
+           $t->unsignedBigInteger("ITEM_ID");
+           $t->unsignedInteger("COUNT");
+           $t->timestamp("PRICE_LAST_UPDATED")->useCurrent()->index();
+           $t->integer("RUN_ID");
+           $t->enum("TYPE", [
+               "RED_LOOT", "BLUEPRINT", "SKILLBOOK", "MATERIAL", "OTHER"
+           ]);
         });
     }
 
@@ -31,6 +47,14 @@ class ExtendRunsInfo extends Migration
      */
     public function down()
     {
-        //
+        Schema::table("runs",function (Blueprint $t) {
+            $t->removeColumn("SHIP_ID");
+            $t->removeColumn("DEATH_REASON");
+            $t->removeColumn("PVP_CONDUIT_USED");
+            $t->removeColumn("PVP_CONDUIT_SPAWN");
+            $t->removeColumn("FILAMENT_PRICE");
+        });
+
+        Schema::dropIfExists("detailed_loot");
     }
 }
