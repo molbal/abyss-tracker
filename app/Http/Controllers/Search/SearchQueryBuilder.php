353d14d89f858f2a93d65fff47c2b476ad96c837
Balint Molnar <molbal@outlook.com>
2020-03-01 23:01:48 +0100

    Adds an laravel query builder function from the search query builder

--- app/Http/Controllers/Search/SearchQueryBuilder.php
+++ app/Http/Controllers/Search/SearchQueryBuilder.php
@@ -2,7 +2,9 @@
 
 	namespace App\Http\Controllers\Search;
 
-	use Illuminate\Support\Collection;
+	use Illuminate\Database\Query\Builder;
+    use Illuminate\Support\Collection;
+    use Illuminate\Support\Facades\DB;
 
     class SearchQueryBuilder {
 
@@ -25,5 +27,31 @@
             $this->conditions->add($condition);
             return $this;
         }
+
+        /**
+         * @return Collection
+         */
+        public function getConditions() : Collection {
+            return $this->conditions;
+        }
+
+        /**
+         * Gets the query builder query
+         * @return Builder
+         */
+        public function getQuery():Builder {
+            $builder = DB::table("runs")
+                         ->leftJoin("detailed_loot", "runs.ID", "=", "detailed_loot.RUN_ID")
+                         ->leftJoin("ship_lookup", "runs.SHIP_ID", "=", "ship_lookup.ID");
+
+            /** @var SearchCriteria $condition */
+            foreach ($this->conditions as $condition) {
+                $builder->where(sprintf("%s.%s", $condition->getTable(), $condition->getField()), $condition->getRelation(), $condition->getValue());
+            }
+
+            return $builder;
+        }
+
+
 
     }
