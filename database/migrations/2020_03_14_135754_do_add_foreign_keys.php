3cbd2f758cc12084ec481e503a3f3acd09a6feaf
Balint Molnar <molbal@outlook.com>
2020-03-20 21:56:21 +0100

    retarded laravel migrations

--- database/migrations/2020_03_14_135754_do_add_foreign_keys.php
+++ database/migrations/2020_03_14_135754_do_add_foreign_keys.php
@@ -13,33 +13,22 @@
          */
         public function up() {
             DB::statement("SET FOREIGN_KEY_CHECKS = 0;");
-            Schema::table("runs", function (Blueprint $table) {
-                $table->foreign("CHAR_ID")->references("CHAR_ID")->on("chars");
-                $table->foreign("SHIP_ID")->references("ID")->on("ship_lookup");
-            });
-            Schema::table("detailed_loot", function (Blueprint $table) {
-                $table->foreign("ITEM_ID")->references("ITEM_ID")->on("item_prices");
-                $table->foreign("RUN_ID")->references("ID")->on("runs");
-            });
-            Schema::table("lost_items", function (Blueprint $table) {
-                $table->foreign("ITEM_ID")->references("ITEM_ID")->on("item_prices");
-                $table->foreign("RUN_ID")->references("ID")->on("runs");
-            });
-            Schema::table("stopwatch", function (Blueprint $table) {
-                $table->foreign("CHAR_ID")->references("CHAR_ID")->on("chars");
-            });
+            try {DB::statement("alter table `runs` add constraint `runs_char_id_foreign` foreign key (`CHAR_ID`) references `chars` (`CHAR_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+            try {DB::statement("alter table `runs` add constraint `runs_ship_id_foreign` foreign key (`SHIP_ID`) references `ship_lookup` (`ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
 
-            Schema::table("run_report", function (Blueprint $table) {
-                $table->foreign("RUN_ID")->references("ID")->on("runs");
-            });
-            Schema::table("droprates_cache", function (Blueprint $table) {
-                $table->foreign("ITEM_ID")->references("ITEM_ID")->on("item_prices");
-            });
-            Schema::table("privacy", function (Blueprint $table) {
-                $table->foreign("CHAR_ID")->references("CHAR_ID")->on("chars");
-            });
+            try {DB::statement("alter table `detailed_loot` add constraint `dl_item_id_foreign` foreign key (`ITEM_ID`) references `item_prices` (`ITEM_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+            try {DB::statement("alter table `detailed_loot` add constraint `dl_run_id_foreign` foreign key (`RUN_ID`) references `runs` (`ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
 
+            try {DB::statement("alter table `lost_items` add constraint `li_item_id_foreign` foreign key (`ITEM_ID`) references `item_prices` (`ITEM_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+            try {DB::statement("alter table `lost_items` add constraint `li_run_id_foreign` foreign key (`RUN_ID`) references `runs` (`ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
 
+            try {DB::statement("alter table `stopwatch` add constraint `stopwatch_char_id_foreign` foreign key (`CHAR_ID`) references `chars` (`CHAR_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+
+            try {DB::statement("alter table `run_report` add constraint `rr_char_id_foreign` foreign key (`REPORTER_CHAR_ID`) references `chars` (`CHAR_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+
+            try {DB::statement("alter table `droprates_cache` add constraint `drc_item_id_foreign` foreign key (`ITEM_ID`) references `item_prices` (`ITEM_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
+
+            try {DB::statement("alter table `privacy` add constraint `privacy_char_id_foreign` foreign key (`CHAR_ID`) references `chars` (`CHAR_ID`)");}catch (Exception $e) {echo $e->getMessage()."<br/>";}
 
             DB::statement("SET FOREIGN_KEY_CHECKS = 1;");
         }
