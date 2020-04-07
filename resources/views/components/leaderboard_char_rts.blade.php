a3aa4458db1f3a9e7b5cabed800c22eb25a4ac2c
Balint Molnar <molbal@outlook.com>
2020-04-07 09:36:37 +0200

    Adds highlighta for your own name in leaderboards

--- resources/views/components/leaderboard_char_rts.blade.php
+++ resources/views/components/leaderboard_char_rts.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td>
-        <a href="{{route("profile.index", ["id" => $item->CHAR_ID])}}">
+        <a href="{{route("profile.index", ["id" => $item->CHAR_ID])}}" class="{{session()->get("login_id") == $item->CHAR_ID ? "text-danger" : "text-dark"}}">
             <img src="https://images.evetech.net/characters/{{$item->CHAR_ID}}/portrait?size=32" alt="" style="width: 24px; height: 24px" class="rounded-circle shadow-sm">
             &nbsp;{{$item->NAME}}
         </a></td>
