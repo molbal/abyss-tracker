629f09a6a1348d5066554a0f9574849c255d6f29
Balint Molnar <molbal@outlook.com>
2020-05-07 12:06:40 +0200

    Nice.

--- resources/views/components/leaderboard_char.blade.php
+++ resources/views/components/leaderboard_char.blade.php
@@ -4,5 +4,5 @@
             <img src="https://images.evetech.net/characters/{{$item->CHAR_ID}}/portrait?size=32" alt="" style="width: 24px; height: 24px" class="rounded-circle shadow-sm">
             &nbsp;{{$item->NAME}}
         </a></td>
-    <td class="text-right">{{$item->COUNT}}</td>
+    <td class="text-right" @if($item->COUNT==420||$item->COUNT==69) data-toggle="tooltip" title="Nice"  @endif>{{$item->COUNT}}</td>
 </tr>
