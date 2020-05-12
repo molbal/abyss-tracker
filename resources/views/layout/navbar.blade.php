728523dda1982a31ea63b4c26f04581ede027572
Balint Molnar <molbal@outlook.com>
2020-05-13 00:21:56 +0200

    Removes navbar dropdown

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -31,14 +31,24 @@
                    href="{{route("leaderboard.index")}}">
                     <img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("leaderboard.index" == Route::currentRouteName())}}/trophy.png"> Leaderboard</a>
             </li>
-            <li class="nav-item dropdown {{"ships_all" == Route::currentRouteName() ||  "fits_all" == Route::currentRouteName() ? "active" : ""}}" style="list-style: none">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarShipDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item">
+                <a class="nav-link {{"ships_all" == Route::currentRouteName() ? "active" : ""}}"
+                   href="{{route("ships_all")}}">
                     <img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("ships_all" == Route::currentRouteName())}}/rocket.png"> Ships</a>
-                <div class="dropdown-menu" aria-labelledby="navbarShipDropdown">
-                    <a class="dropdown-item pl-2 {{"ships_all" == Route::currentRouteName() ? "active text-dark" : ""}}" href="{{route("ships_all")}}"><img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("ships_all" == Route::currentRouteName())}}/rocket.png" class="mr-1"/> Ships</a>
-                    <a class="dropdown-item pl-2 {{"fit.index" == Route::currentRouteName() ? "active text-dark" : ""}}" href="{{route("fit.index")}}"><img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("fit.index" == Route::currentRouteName())}}/job.png" class="mr-1"/> Fits</a>
-                 </div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link {{"fit.index" == Route::currentRouteName() ? "active" : ""}}"
+                   href="{{route("fit.index")}}">
+                    <img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("fit.index" == Route::currentRouteName())}}/job.png"> Fits</a>
+            </li>
+{{--            <li class="nav-item dropdown {{"ships_all" == Route::currentRouteName() ||  "fits_all" == Route::currentRouteName() ? "active" : ""}}" style="list-style: none">--}}
+{{--                <a class="nav-link dropdown-toggle" href="#" id="navbarShipDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--                    <img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("ships_all" == Route::currentRouteName())}}/rocket.png"> Ships</a>--}}
+{{--                <div class="dropdown-menu" aria-labelledby="navbarShipDropdown">--}}
+{{--                    <a class="dropdown-item pl-2 {{"ships_all" == Route::currentRouteName() ? "active text-dark" : ""}}" href="{{route("ships_all")}}"><img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("ships_all" == Route::currentRouteName())}}/rocket.png" class="mr-1"/> Ships</a>--}}
+{{--                    <a class="dropdown-item pl-2 {{"fit.index" == Route::currentRouteName() ? "active text-dark" : ""}}" href="{{route("fit.index")}}"><img src="https://img.icons8.com/ios-glyphs/16/{{App\Http\Controllers\ThemeController::getThemedNavBarIconColor("fit.index" == Route::currentRouteName())}}/job.png" class="mr-1"/> Fits</a>--}}
+{{--                 </div>--}}
+{{--            </li>--}}
             <li class="nav-item">
                 <a class="nav-link {{"changelog" == Route::currentRouteName() ? "active" : ""}}"
                    href="{{route("changelog")}}">
