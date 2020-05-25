6937daf1a2fc9ec8b86da86f21f97b54ff5663dd
Balint Molnar <molbal@outlook.com>
2020-05-25 21:54:33 +0200

    Adds median controller and break even calculator controller

--- app/Http/Controllers/DS/MedianController.php
+++ app/Http/Controllers/DS/MedianController.php
@@ -22,5 +22,18 @@
             return Cache::remember(sprintf("aft.median.fit.%s.%d.%s", $fitId, $tier, $type), now()->addMinute(), function () use ($fitId, $tier, $type) {
                 return intval(DB::select("select MEDIAN_FOR_FIT_TYPE_TIER(?, ?, ?) as `MEDIAN`;", [$fitId, $tier, $type])[0]->MEDIAN);
             });
-	    }
+        }
+        /**
+         * Gets a median runtime value for a ship (cached for up to a minute)
+         * @param int $fitId fit ID
+         * @param int $tier tier
+         * @param int $type type
+         *
+         * @return int
+         */
+        public static function getFitMedianRuntime(int $fitId, int $tier, string $type): int {
+            return Cache::remember(sprintf("aft.median.fit.%s.%d.%s", $fitId, $tier, $type), now()->addMinute(), function () use ($fitId, $tier, $type) {
+                return intval(DB::select("select MEDIAN_FOR_FIT_TYPE_TIER_RUNTIME(?, ?, ?) as `MEDIAN`;", [$fitId, $tier, $type])[0]->MEDIAN);
+            });
+        }
 	}
