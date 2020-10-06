0eee0c1a34492ee763004b635cffa0ace9def191
Balint Molnar <molbal@outlook.com>
2020-10-06 21:51:00 +0200

    Adds fit_logs table

--- database/migrations/2020_10_05_205814_fits_table_add_root_id_up_to_date_revision.php
+++ database/migrations/2020_10_05_205814_fits_table_add_root_id_up_to_date_revision.php
@@ -4,8 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Schema;
+    use Symfony\Component\Console\Output\ConsoleOutput;
 
-class FitsTableAddRootIdUpToDateRevision extends Migration
+    class FitsTableAddRootIdUpToDateRevision extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +15,16 @@ class FitsTableAddRootIdUpToDateRevision extends Migration
      */
     public function up()
     {
+        $consoleOutput = new ConsoleOutput();
         Schema::table('fits', function (Blueprint $table) {
             $table->unsignedBigInteger("ROOT_ID")->nullable(true)->index();
             $table->unsignedInteger("REVISION_NUMBER")->default('0');
             $table->enum('LAST_PATCH', ['untested', 'works', 'deprecated'])->default('untested');
+            $table->timestamp('CREATED_AT')->nullable(true);
         });
+        $consoleOutput->writeln("Altered fits table");
         DB::unprepared("update fits set ROOT_ID=ID");
+        $consoleOutput->writeln("Set fit root IDs on existing records");
     }
 
     /**
@@ -30,7 +35,7 @@ class FitsTableAddRootIdUpToDateRevision extends Migration
     public function down()
     {
         Schema::table('fits', function (Blueprint $table) {
-            $table->dropColumn(['ROOT_ID','REVISION_NUMBER','LAST_PATCH']);
+            $table->dropColumn(['ROOT_ID','REVISION_NUMBER','LAST_PATCH', 'CREATED_AT']);
         });
     }
 }
